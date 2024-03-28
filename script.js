47157422897244a48b0ec3b2dc2c61b9e0b090f1
dantvi <daniel.tving@gmail.com>
2024-03-28 20:16:36 +0100

    change volume

--- script.js
+++ script.js
@@ -56,8 +56,28 @@ function setProgress(e) {
 }
 
 // Volume Controls -------------------------------- //
-
-
+// Volume Bar
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    // Rounding volume up or down
+    if (volume < 0.1) {
+        volume = 0;
+    }
+    if (volume > 0.9) {
+        volume = 1;
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+    // Change icon depending on volume
+    volumeIcon.className = '';
+    if (volume > 0.7) {
+        volumeIcon.classList.add('fa-solid', 'fa-volume-high');
+    } else if (volume < 0.7 && volume > 0) {
+        volumeIcon.classList.add('fa-solid', 'fa-volume-low');
+    } else if (volume === 0) {
+        volumeIcon.classList.add('fa-solid', 'fa-volume-off');
+    }
+}
 
 // Change Playback Speed -------------------------- //
 
@@ -72,3 +92,4 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
