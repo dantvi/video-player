16bfdc36fe54a2ab5630af226f37505c326bc222
dantvi <daniel.tving@gmail.com>
2024-03-30 11:08:13 +0100

    playback speed

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
+const speed = document.querySelector('.player-speed');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 
@@ -54,6 +55,7 @@ function setProgress(e) {
     video.currentTime = newTime * video.duration;
 }
 
+
 // Volume Controls -------------------------------- //
 let lastVolume = 1;
 
@@ -100,7 +102,9 @@ function toggleMute() {
 
 
 // Change Playback Speed -------------------------- //
-
+function changeSpeed() {
+    video.playbackRate = speed.value;
+}
 
 
 // Fullscreen ------------------------------------- //
@@ -114,3 +118,4 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
